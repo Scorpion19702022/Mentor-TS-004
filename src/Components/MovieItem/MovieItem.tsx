422981a9be402fbd3c0744ca0192a1ab0f12966e
skorpion <jmosinski@o2.pl>
2024-05-29 22:30:28 +0200

    test

--- src/Components/MovieItem/MovieItem.tsx
+++ src/Components/MovieItem/MovieItem.tsx
@@ -1,14 +1,16 @@
 import styles from './MovieItem.module.css'
 
+import Helicopter from '../../assets/helikopter.jpg'
+
 const MovieItem = () => {
 	return (
 		<div className={styles.wrapper}>
-			<img src='' alt='' />
-			<h4>Helikopter w ogniu</h4>
-			<span>ocena: 10</span>
-			<div>
-				<button>zobacz</button>
-				<button>add to</button>
+			<img src={Helicopter} alt='moviePhoto' />
+			<h4 className={styles.heading_movie}>Helikopter w ogniu</h4>
+			<span className={styles.rating}>ocena: 10</span>
+			<div className={styles.box_btns}>
+				<button className={styles.btn}>zobacz</button>
+				<button className={styles.btn}>add to</button>
 			</div>
 		</div>
 	)
