8dd33b8e244e307b81ca35ec5f0ae644fc787e6f
skorpion <jmosinski@o2.pl>
2024-05-29 22:39:37 +0200

    test

--- src/Components/MovieItem/MovieItem.tsx
+++ src/Components/MovieItem/MovieItem.tsx
@@ -5,12 +5,18 @@ import Helicopter from '../../assets/helikopter.jpg'
 const MovieItem = () => {
 	return (
 		<div className={styles.wrapper}>
-			<img src={Helicopter} alt='moviePhoto' />
-			<h4 className={styles.heading_movie}>Helikopter w ogniu</h4>
-			<span className={styles.rating}>ocena: 10</span>
-			<div className={styles.box_btns}>
-				<button className={styles.btn}>zobacz</button>
-				<button className={styles.btn}>add to</button>
+			<div className={styles.box_movies}>
+				<div className={styles.box_img}>
+					<img className={styles.img_movie} src={Helicopter} alt='moviePhoto' />
+				</div>
+				<div className={styles.box_content}>
+					<h4 className={styles.heading_movie}>Helikopter w ogniu</h4>
+					<span className={styles.rating}>ocena: 10</span>
+					<div className={styles.box_btns}>
+						<button className={styles.btn}>zobacz</button>
+						<button className={styles.btn}>add to</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
