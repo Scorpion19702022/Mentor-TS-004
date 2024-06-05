43019eeec77f77c15ed7e50e5f2240f32d6cc08c
skorpion <jmosinski@o2.pl>
2024-06-05 19:14:29 +0200

    test

--- src/Components/Bestsellers/Bestsellers.tsx
+++ src/Components/Bestsellers/Bestsellers.tsx
@@ -1,14 +1,16 @@
 import useTopRated from '../../Hooks/useTopRated'
 import MovieItem from '../MovieItem/MovieItem'
 
+import styles from './Bestsellers.module.css'
+
 const Bestsellers = () => {
 	const { topRated } = useTopRated(3)
 
 	const topType = topRated.map((item: any) => <MovieItem movieProps={item} />)
 
 	return (
-		<section>
-			<h2>Bestsellers</h2>
+		<section className={styles.wrapper}>
+			<h2 className={styles.heading}>Bestsellers</h2>
 			<div>{topType}</div>
 		</section>
 	)
