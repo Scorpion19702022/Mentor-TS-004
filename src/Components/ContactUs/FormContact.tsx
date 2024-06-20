6ff74bcf45841f7ed67e29d3e3865d754e457744
skorpion <jmosinski@o2.pl>
2024-06-20 20:33:31 +0200

    test

--- src/Components/ContactUs/FormContact.tsx
+++ src/Components/ContactUs/FormContact.tsx
@@ -1,15 +1,44 @@
+import { useRef } from 'react'
 import styles from './Contact.module.css'
 
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+import emailjs from '@emailjs/browser'
+
+type Input = {
+	title: string
+	content: string
+	email: string
+}
+
 const FormContact = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Input>()
+	const onSubmit: SubmitHandler<Input> = data => {
+		console.log(data)
+		emailjs.sendForm('', 'template_hey92tp', form.current, 'xbLltySyebCSexAaR').then(
+			(result: any) => {
+				console.log(result)
+			},
+			(error: any) => console.log(error)
+		)
+	}
+
+	const form: any = useRef()
+
 	return (
-		<form className={styles.wrapper_form}>
+		<form className={styles.wrapper_form} onSubmit={handleSubmit(onSubmit)} ref={form}>
 			<div className={styles.box_form}>
 				<label className={styles.label}>Wpisz tytuł wiadomości:"</label>
-				<input className={styles.input} type='text' />
+				<input className={styles.input} type='text' {...register('title')} />
 				<label className={styles.label}>Wpisz wiadomość:</label>
-				<textarea className={styles.textarea}></textarea>
+				<textarea className={styles.textarea} {...register('content')}></textarea>
 				<label className={styles.label}>Podaj adres email:</label>
-				<input className={styles.input} type='text' />
+				<input className={styles.input} type='text' {...register('email')} />
 			</div>
 		</form>
 	)
