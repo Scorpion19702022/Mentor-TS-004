6f2818e6755c0d38a759ce3dfb656f08b5abf6c2
skorpion <jmosinski@o2.pl>
2024-07-04 20:19:27 +0200

    test

--- src/Components/ContactUs/FormContact.tsx
+++ src/Components/ContactUs/FormContact.tsx
@@ -4,6 +4,7 @@ import styles from './Contact.module.css'
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 import emailjs from '@emailjs/browser'
+import { error } from 'console'
 
 type Input = {
 	title: string
@@ -12,6 +13,30 @@ type Input = {
 }
 
 const FormContact = () => {
+	const registerOptions = {
+		title: {
+			required: `pole jest wymagane`,
+			minLength: {
+				value: 10,
+				message: 'za mało zaków',
+			},
+			maxLength: {
+				value: 20,
+				message: 'za dużo znaków',
+			},
+		},
+		content: {
+			required: 'pole jest wymagane',
+			maxLength: {
+				value: 100,
+				message: 'za dużo znaków',
+			},
+		},
+		email: {
+			required: 'pole wymagane',
+		},
+	}
+
 	const {
 		register,
 		handleSubmit,
@@ -21,7 +46,7 @@ const FormContact = () => {
 	} = useForm<Input>()
 	const onSubmit: SubmitHandler<Input> = (data: Input) => {
 		console.log(data)
-		emailjs.sendForm('service_w77dd6g', 'template_hey92tp', data, 'xbLltySyebCSexAaR').then(
+		emailjs.sendForm('service_w77dd6g', 'template_hey92tp', form.current, 'xbLltySyebCSexAaR').then(
 			(result: any) => {
 				console.log(result.text)
 			},
@@ -36,11 +61,26 @@ const FormContact = () => {
 		<form className={styles.wrapper_form} onSubmit={handleSubmit(onSubmit)} ref={form}>
 			<div className={styles.box_form}>
 				<label className={styles.label}>Wpisz tytuł wiadomości:"</label>
-				<input className={styles.input} type='text' {...register('title')} />
+				<input className={styles.input} type='text' {...register('title', registerOptions.title)} />
+				{errors.title?.message ? (
+					<span className={styles.errors}>{errors.title?.message}</span>
+				) : (
+					<span className={styles.noErrors}></span>
+				)}
 				<label className={styles.label}>Wpisz wiadomość:</label>
-				<textarea className={styles.textarea} {...register('content')}></textarea>
+				<textarea className={styles.textarea} {...register('content', registerOptions.content)}></textarea>
+				{errors.content?.message ? (
+					<span className={styles.errors}>{errors.content?.message}</span>
+				) : (
+					<span className={styles.noErrors}></span>
+				)}
 				<label className={styles.label}>Podaj adres email:</label>
-				<input className={styles.input} type='text' {...register('email')} />
+				<input className={styles.input} type='text' {...register('email', registerOptions.email)} />
+				{errors.email?.message ? (
+					<span className={styles.errors}>{errors.email?.message}</span>
+				) : (
+					<span className={styles.noErrors}></span>
+				)}
 				<button type='submit'>Wyślij</button>
 			</div>
 		</form>
